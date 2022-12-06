b01bf5172ebebb0d9f77509784c78b43fa8e60d2
Ahmet Arif <karakullukcuahmetarif@gmail.com>
2022-12-06 12:02:54 +0300

    test

--- src/Test.cpp
+++ src/Test.cpp
@@ -11,49 +11,59 @@ using namespace std;
 
 int main(){
     
-int counter=0;
 
+int counter =0;
+
+System* system = new System ();
 Organ* organ = new Organ();
-System* system = new System();
+
+
 
 ifstream in;
     in.open("Veri.txt");
-    while (!in.eof())
-    {
 
-        string line;
-        getline(in, line);
-        
-        if (in.fail())
-        {
-            break;
-        }
-        
-        stringstream ss;
-        ss << line;
-        int sonraki;
-
-        Tissue* tissue = new Tissue();
-     
-        while (bool(ss>>sonraki)==true)
-        {
-            tissue->add(sonraki);
-                  
-        }
-
-        Radix* radix = new Radix(tissue);
-        tissue->midNumber=radix->Sort();
-        delete radix;
-        if(counter%20==0 && counter!=0){
-           Organ* organ = new Organ();
-        }
-        organ->bst->Add(tissue);
-
-        system->add(organ);
-        
-        counter++;
-   }
-   organ->bst->preorder();
-   cout<<"helloworld";
    
-}
+            while (!in.eof())
+            {
+
+                string line;
+                getline(in, line);
+                
+                if (in.fail())
+                {
+                    break;
+                }
+                
+                stringstream ss;
+                ss << line;
+                int sonraki;
+
+                Tissue* tissue = new Tissue();
+            
+                while (bool(ss>>sonraki)==true)
+                {
+                    tissue->add(sonraki);
+                        
+                }
+
+                Radix* radix = new Radix(tissue);
+                tissue->midNumber=radix->Sort();
+                delete radix;
+
+                if(counter%20==0)
+                {
+                    system->add(organ);
+                    Organ* organ = new Organ();
+                }
+                organ->bst->Add(tissue);
+
+            
+                counter++;
+                
+            }
+            
+        }
+    
+     
+   
+   
